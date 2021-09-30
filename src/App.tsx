a377db90beea830ac329256e251ea369ee925b84
magnram <magnus.ramm@gmail.com>
2021-09-30 09:27:54 +0200

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-import { Redirect } from "react-router-dom";
 import './App.css';
 
 function App() {
-  const [disabled, setDisabled] = useState(false);
   window.location.href = "http://192.168.0.12:3000"
   return (
     <div>
